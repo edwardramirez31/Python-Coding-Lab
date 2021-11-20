2e98bb5969e8a8df57de14f5fba97442cb2d571f
edwardramirez31 <66492063+edwardramirez31@users.noreply.github.com>
2021-11-20 12:51:07 -0500

    Adding first week class examples

---
+++ Semana1/ejemplo1.py
@@ -0,0 +1,11 @@
+# necesito un programa que le pida el nombre al usuario y despues lo salude por ese nombre
+
+# INPUT: Nombre del usuario
+# 1. Obtener el nombre
+nombre_del_usuario = input("Ingrese su nombre de usuario: ")
+# 2. Formatear el mensaje
+message = "Hola, " + nombre_del_usuario
+# 3. Mostrarlo al usuario
+print(message)
+
+# OUTPUT: Saludo con el nombre
---
+++ Semana1/hello_world.py
@@ -0,0 +1,98 @@
+# NECESITO LA EDAD DEL USUARIO PARA PRODUCIR LA EDAD QUE TENDRA EN EL SIGUIENTE AÑO
+
+# INPUT: Edad de la persona ? Restricciones: edad > 0
+# 1. Pedir la edad al usuario
+while True:
+    try:
+        edad_del_usuario = int(input("Ingrese su edad: "))
+        if edad_del_usuario > 0:
+            break
+        else:
+            print("Ingrese una edad valida")
+    except:
+        print("Ingrese una edad valida")
+# 2. Calcular la edad del usuario en el siguiente año (edad + 1)
+edad_mas_uno = edad_del_usuario + 1
+# 3. Mostrar el resultado al usuario
+print("Su edad dentro de un año será: " + str(edad_mas_uno))
+# OUTPUT: Edad de la persona + 1
+
+
+# Catalina Suarez8:03
+# 2180136 J1
+# Natalia Amaya8:04
+# 2184155 J1
+# Andres Banderas Uribe8:04
+# 2184597 grupo j1
+# Mateo8:04
+# 2190175-J1
+# Ricardo Andres Galeano Beltran8:04
+# 2194519 J1
+# Jesus David Caballero8:04
+# 2191807 J1
+# William Steven Acevedo Rueda8:04
+# William Steven Acevedo Rueda-2192059-D3
+# jonathan stiven rodriguez rincon8:04
+# Jonatan Rodríguez 2200269 grupo: D2
+# Cristian David Arias Salazar8:04
+# 2174520 H2
+# Carlos Andres Figueroa Vega8:04
+# carlos figueroa 2180929 grupo H2
+# Naren Herrera8:04
+# Naren Jadir Herrera Amado_2180158_Grupo D2
+# Mariam Cassiani8:05
+# 2194509 grupo D2
+# Andres Moreno8:05
+# Fabian Andrés Moreno Saavedra 2192092 - J1
+# Jackson Ricardo Moncada Sanchez8:05
+# Jackson Moncada 2200299 D2
+# Octavio Rodríguez8:05
+# Octavio Rodriguez-2192054-J1
+# juan camilo rios gonzalez8:05
+# Juan camilo rios gonzalez-2192057-J1
+# William Steven Acevedo Rueda8:05
+# 2192059 grupo D3 (benjumea)
+# carlos olivares8:05
+# 2184510 H2
+# Angie Castañeda8:06
+# Angie Lizeth Fontecha Castañeda-2195506, j1
+# EDWIN MANRIQUE CASTAÑEDA8:06
+# Edwin Manrique Castañeda 2122884 - J1
+# Andres Ramos8:06
+# Carlos Andres Ramos Vargas 2182141 H2
+# Dadvinson Bonilla8:06
+# Dadvinson Bonilla Gualdrón - 2180178 - D2
+# Tú8:06
+# https://classroom.google.com/c/NDMyNDY5ODExMDY4?hl=es&cjc=sd65soo
+# carlos arenas8:08
+# carlos mauricio arenas rodriguez - 2192063 - J1
+# Carmen Cecilia Rodriguez Ramirez8:08
+# Carmen Cecilia Rodríguez Ramírez- 2192107 - J1
+# omar pabon8:08
+# Omar Yesid Pabon Pabon - 2194042 J1
+# Paula Jimena Galvis Rueda8:08
+# Paula Jimena Galvis Rueda - 2181253 J1
+# Natalia Acevedo8:12
+# 2190168 - H1
+# Tú8:26
+# He compartido un archivo jam con la reunión: https://jamboard.google.com/d/1c7iHYSj1Vljmxq6-1u3d2ySwhugOywcYlxXYd_nQUBU/edit?usp=meet_whiteboard
+# Silvia Diaz8:35
+# Silvia Diaz-2192570-J1
+# William Steven Acevedo Rueda8:40
+# 255?
+# Andres Moreno8:40
+# si, 255
+# Andres Moreno8:43
+# 85
+# Andres Moreno10:02
+# Python también tiene la función msgbox?
+# Andres Moreno10:06
+# eso lo veremos tambien?
+# Natalia Acevedo10:07
+# las clases las grabas?
+# Eusebio Ramirez10:07
+# eusebio enriqye ramirez 2192099
+# Andres Moreno10:09
+# gracias
+# wilson esneider sierra mayorga10:09
+# wilson sierra mayorga 2184599 j1
