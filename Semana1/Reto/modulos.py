f630452ddc7ad5010cdb2c25f46025262366fe29
edwardramirez31 <edal_ramirez@hotmail.com>
2021-11-23 14:39:27 -0500

    Week One Challenge Introduction

---
+++ Semana1/Reto/modulos.py
@@ -0,0 +1,15 @@
+from math import pi, sqrt
+
+# SINTAXIS:
+# from nombre_del_modulo import nombre_de_mi_funcion, nombre_de_mi_otra_funcion, ...
+from retos import longitud, message
+
+print(message + " mundo")
+
+print(pi * 1.1 ** 2)
+
+numero = 9
+
+raiz = sqrt(9)
+
+print(raiz * longitud)
---
+++ Semana1/Reto/retos.py
@@ -0,0 +1,17 @@
+# SLICING
+
+# H O L A
+# 0 1 2 3
+
+message = "Hola"
+
+longitud = len(message)
+
+# print(message[longitud - 1])
+
+# print(message[0:3:2])
+
+# lista = ["documento", "word"]
+
+# print(lista[0])
+# print(lista[-1])
