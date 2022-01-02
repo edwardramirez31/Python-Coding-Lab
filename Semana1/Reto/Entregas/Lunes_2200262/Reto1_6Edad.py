c394ecdfecc9ad645953dfa852623d2d119881bc
brayanparra04 <97001276+brayanparra04@users.noreply.github.com>
2022-01-02 17:53:39 -0500

    Reto Semana 1 Solución
    Código: 2200262
    Brayan Esneider Parra Gómez
    Grupo : D3
    Profesor: José Benjumea

---
+++ Semana1/Reto/Entregas/Lunes_2200262/Reto1_6Edad.py
@@ -0,0 +1,5 @@
+print("Vamos a descubrir cuándo cumplirás 100 años")
+nombre = input("Ingresa tu nombre ")
+edad = input("Ingresa tu edad ")
+cuando_cumpla_100_años = 2121-int(edad)
+print(f"Cumplirás 100 años en {cuando_cumpla_100_años}")
